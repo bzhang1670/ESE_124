542142198c6cb1cb2844c5292b894e0178c1d916
Brian Zhang <brianzhang@Brians-MacBook-Pro.local>
2014-03-03 08:36:40 -0500

    Updated

--- HW2.c
+++ HW2.c
@@ -16,10 +16,10 @@
  Program 2:
  Modify your Program 1 above so that, when executed, it will do the following in the indicated sequence:
   1. Ask the user to enter on the keyboard a starting value nstart of a positive     integer number n.
-  3. Ask the user to enter on the keyboard the ending value nstop of n.
-  4. Ask the user to enter on the keyboard the increment nstep in n from one 
+  2. Ask the user to enter on the keyboard the ending value nstop of n.
+  3. Ask the user to enter on the keyboard the increment nstep in n from one 
      value to the next.
-  5. The program is then to display on the console a table consisting of two 
+  4. The program is then to display on the console a table consisting of two 
      columns, one displaying values of n starting at n1 and the other displaying     the corresponding zNumber values. Make sure that your display includes 
      suitable headers for the two columns. 
 
@@ -35,21 +35,29 @@
 
 
 int main() {
-  int input, nstart, nstop;
+  int input, nstart, nstop, nstep;
 
   printf("Enter an integer to calculate the zNumber of: \n");
   scanf("%d", &input);
-  printf("The zNumber of  %d + is zNumber(%d) = %d: \n ", input, input, zNumber(input));
+  printf("The zNumber of  %d is zNumber(%d) = %d: \n ", input, input, zNumber(input));
   
   printf("\nEnter the start integer of the interval to calculate the zNumber for: \n");
   scanf("%d", &nstart);
   printf("\nNow enter the stop integer: \n");
   scanf("%d", &nstop);
-  printf("\nzNumber2(%d,%d) = %d \n", nstart, nstop, zNumber2(nstart,nstop));
+  printf("\nNow enter the increment step: \n");
+  scanf("%d", &nstep);
+  int i = 0;
+  
+  printf("\nn |zNumber2(n)\n");
+  for(i = nstart; i <= nstop; i+=nstep){ 
+    printf("\n%d |%d\n",i,zNumber(i));
+  }
 
   getchar();
   return 0;
 }
+
 //Program 1
 int zNumber(int input) {
   int answer = 0;
@@ -62,14 +70,3 @@ int zNumber(int input) {
   return answer;
 }
 
-//Program 2 (Modified Program 1)
-int zNumber2(int nstart, int nstop) {
-  int answer = 0;
-  int j;
-
-  for(j = nstart; j <= nstop; j++){
-    answer += pow(j,3);
-  }
-
-  return answer;
-}
