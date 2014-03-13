fe71ba90d74c79ac90a097487dc3e2b5fda704ce
Brian Zhang <brianzhang@Brians-MacBook-Pro.local>
2014-03-13 10:14:06 -0400

    Uploaded HW 4

--- HW4.c
+++ HW4.c
@@ -1,10 +1,21 @@
+/****************
+Brian Zhang
+ESE 124
+HW 4 
+Due: 3/13/14
+
+Assignment: Design a program that takes userinput for runmode for a circuit (series or parallel), and then evaluates the equivalent resistance for that circuit.
+
+ **************/
+
+
 #include<stdio.h>
 #include<math.h>
 
 int main() {
   
   int i, runmode, numres, res, try;
-  float sum =0;
+  float Req=0;
   printf("Enter a integer for runmode (0 for series, 1 for parallel): \n");
   while(try < 1) {
     scanf("%d",&runmode);
@@ -17,7 +28,8 @@ int main() {
       for(i=0; i<numres; i++) {
 	printf("Enter resistance[%d]:\n", i+1);
 	scanf("%d", &res);
-	sum += (float) res;
+	if (res >= 0)
+	  Req += (float) res;
 	
       } //end forloop
 
@@ -31,10 +43,11 @@ int main() {
       for(i=0; i<numres; i++) {
 	printf("Enter resistance[%d]:\n", i+1);
 	scanf("%d", &res);
-	sum += 1.0 / ((float) res);
+	if(res >= 0)
+	  Req += 1.0 / ((float) res);
 	
       } //end forloop
-      sum = 1/sum;
+      Req = 1/Req;
       
       try++;
       
@@ -46,7 +59,7 @@ int main() {
     
   } //end while loop
 
-  printf("The total resistances of your circuit is %.5f Ohms \n", sum);
+  printf("The total resistances of your circuit is %.5f Ohms \n", Req);
 
   getchar();
   return 0;
